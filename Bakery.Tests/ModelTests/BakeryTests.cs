3c12ba87cabd806c61188ed8c5c49f7514985203
Elena <elenarosamail@gmail.com>
2023-09-09 17:15:03 -0700

    pass instance of class test

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+
+  public class BreadTests
+ {
+    [TestMethod]
+    public void BreadConstructor_CreateInstanceOfBread_Bread()
+     {
+        Bread newBread = new Bread();
+        Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+ }
+} 
